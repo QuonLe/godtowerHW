e4c3d43812f8108be593bf574bf01ecf78d2c6a1
QUONLE\quanle <quanlmt08.sic@gmail.com>
2017-09-21 04:16:43 +0700

    hihi

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	// UI Components
+	public Text hintText;
+	public InputField InputField;
+	public Text levelText;
+
+	// Normal variables
+	public string levelAnswer;
+	public string levelNumber;
+	public string levelHint;
+	public string nextScene;
+	public string hintScene;
+
+
+
+	// Use this for initialization
+	void Start () {
+		hintText.text = levelHint;
+
+	}
+
+	public void checkAnswer(){
+		if (InputField.text == levelAnswer) {
+			// TODO : Change Scene
+			hintText.text = "good blow job";
+			hintText.color = Color.green;
+			SceneManager.LoadScene (nextScene);
+		} else {
+			hintText.text = "ACCESS DENIED!!!";
+			hintText.color = Color.red;
+			InputField.text = "";
+			InputField.ActivateInputField ();
+
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void nextButton(){
+		SceneManager.LoadScene (hintScene);
+	}
+}
+
